e2e29fdf70d89d8ce09a56a7c7168fe8fe4b2e81
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-10 20:59:46 +0530

    Starter files

---
+++ src/content/content.js
@@ -0,0 +1,38 @@
+// Basic
+// &dollar;19.99
+// &dollar;199.99
+// 500 GB Storage
+// 2 Users Allowed
+// Send up to 3 GB
+// Learn More
+
+// Professional
+// &dollar;24.99
+// &dollar;249.99
+// 1 TB Storage
+// 5 Users Allowed
+// Send up to 10 GB
+// Learn More
+
+// Master
+// &dollar;39.99
+// &dollar;399.99
+// 2 TB Storage
+// 10 Users Allowed
+// Send up to 20 GB
+// Learn More
+
+// # Front-end Style Guide
+// ## Colors
+
+// ### Primary
+// - Linear Gradient: hsl(236, 72%, 79%) to hsl(237, 63%, 64%)
+
+// ### Neutral
+// - Very Light Grayish Blue: hsl(240, 78%, 98%)
+// - Light Grayish Blue: hsl(234, 14%, 74%)
+// - Grayish Blue: hsl(233, 13%, 49%)
+// - Dark Grayish Blue: hsl(232, 13%, 33%)
+
+// - Family: [Montserrat](https://fonts.google.com/specimen/Montserrat)
+// - Weight: 700
