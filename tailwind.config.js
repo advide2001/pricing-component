8ba243a1796e903989f00b0b1a8a268b974864c7
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-11 12:59:53 +0530

    customized tailwind by making changes in config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
+      fontWeight: {
+        normal: 400,
+        bold: 700,
+      },
+      colors: {
+        lavender: {
+          light: "hsl(236, 72%, 79%)",
+          dark: "hsl(237, 63%, 64%)",
+        },
+        grayishBlue: {
+          veryLight: "hsl(240, 78%, 98%)",
+          light: "hsl(234, 14%, 74%)",
+          DEFAULT: "hsl(233, 13%, 49%)",
+          dark: "hsl(232, 13%, 33%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
