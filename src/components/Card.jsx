dc01b05d231f647875449d92e43e88b1327cf132
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-11 20:55:46 +0530

    Rendering popular plans differently

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,31 @@ import Button from "./Button";
 
 const Card = (props) => {
   return (
-    <div className="bg-white rounded-xl shadow-lg px-8 py-10 text-center">
-      <h2 className="text-grayishBlue font-bold">{props.plan.name}</h2>
-      <p className="text-grayishBlue-dark text-4xl font-bold my-6 flex items-center justify-center">
+    <div
+      className={` rounded-xl shadow-lg px-8 py-10 text-center ${
+        props.plan.isRecommended
+          ? "text-white bg-black bg-gradient-to-b from-lavender-light to-lavender-dark"
+          : "text-grayishBlue-dark bg-white"
+      }
+      `}>
+      <h2 className=" font-bold">{props.plan.name}</h2>
+      <p className=" text-4xl font-bold my-6 flex items-center justify-center">
         <span>$</span>
         <span className="text-6xl ml-2">{props.plan.price}</span>
       </p>
-      <div className="flex flex-col divide-y-2 divide-grayishBlue-light border-t-2 border-b-2 border-grayishBlue-light">
+      <div
+        className={`flex flex-col divide-y-2 border-t-2 border-b-2 ${
+          props.plan.isRecommended
+            ? "divide-grayishBlue-veryLight border-grayishBlue-veryLight"
+            : "divide-grayishBlue-light border-grayishBlue-light"
+        }`}>
         {props.plan.features.map((feature, index) => {
           return (
             <p
               key={index}
-              className="text-grayishBlue text-sm font-bold w-full py-4">
+              className={`${
+                props.plan.isRecommended ? "text-white" : "text-grayishBlue"
+              }  text-sm font-bold w-full py-4`}>
               {feature}
             </p>
           );
