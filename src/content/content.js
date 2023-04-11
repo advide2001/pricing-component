8fe78427c3fdc30f0163cfc1c7e230ab785df43c
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-11 13:00:31 +0530

    Defined content for the page

--- src/content/content.js
+++ src/content/content.js
@@ -1,38 +1,32 @@
-// Basic
-// &dollar;19.99
-// &dollar;199.99
-// 500 GB Storage
-// 2 Users Allowed
-// Send up to 3 GB
-// Learn More
-
-// Professional
-// &dollar;24.99
-// &dollar;249.99
-// 1 TB Storage
-// 5 Users Allowed
-// Send up to 10 GB
-// Learn More
-
-// Master
-// &dollar;39.99
-// &dollar;399.99
-// 2 TB Storage
-// 10 Users Allowed
-// Send up to 20 GB
-// Learn More
-
-// # Front-end Style Guide
-// ## Colors
-
-// ### Primary
-// - Linear Gradient: hsl(236, 72%, 79%) to hsl(237, 63%, 64%)
-
-// ### Neutral
-// - Very Light Grayish Blue: hsl(240, 78%, 98%)
-// - Light Grayish Blue: hsl(234, 14%, 74%)
-// - Grayish Blue: hsl(233, 13%, 49%)
-// - Dark Grayish Blue: hsl(232, 13%, 33%)
-
-// - Family: [Montserrat](https://fonts.google.com/specimen/Montserrat)
-// - Weight: 700
+export const plans = [
+  {
+    id: 1,
+    name: "Basic",
+    price: 19.99,
+    annualPrice: 199.99,
+    storage: "500 GB",
+    usersAllowed: 2,
+    sendLimit: "3 GB",
+    isRecommended: false,
+  },
+  {
+    id: 2,
+    name: "Professional",
+    price: 24.99,
+    annualPrice: 249.99,
+    storage: "1 TB",
+    usersAllowed: 5,
+    sendLimit: "10 GB",
+    isRecommended: true,
+  },
+  {
+    id: 3,
+    name: "Master",
+    price: 39.99,
+    annualPrice: 399.99,
+    storage: "2 TB",
+    usersAllowed: 10,
+    sendLimit: "20 GB",
+    isRecommended: false,
+  },
+];
