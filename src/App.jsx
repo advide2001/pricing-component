0cb329ca2c65b5fe33e9af981a64d30778989e5b
Adarsh Desai <desaiadarsh22@gmail.com>
2023-04-11 20:55:58 +0530

    minor changes in spaces

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import plans from "./content/content";
 
 function App() {
   return (
-    <div className="App mx-8">
+    <div className="App mx-6">
       <h1 className="text-grayishBlue text-3xl text-center font-bold my-12">
         Our Pricing
       </h1>
-      <div className="grid gap-4">
+      <div className="grid gap-8">
         {plans.map((plan) => {
           return <Card key={plan.id} plan={plan} />;
         })}
